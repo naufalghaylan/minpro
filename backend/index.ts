e3dcfe3d1a1c00d924b5f20d45d2eaae2e261852
naufalghaylan <naufalghaylan@gmail.com>
2026-05-04 09:25:25 +0700

    prepare for deployment

--- backend/index.ts
+++ backend/index.ts
@@ -104,7 +104,7 @@ const upload = multer({ storage });
 // ==================
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   })
 );
--- frontend/src/pages/transaksi/TransaksiPage.tsx
+++ frontend/src/pages/transaksi/TransaksiPage.tsx
@@ -22,7 +22,7 @@ export default function TransactionPage() {
 
   const [timeLeft, setTimeLeft] = useState("");
 
-  const BASE_URL = "http://localhost:3000";
+  const BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:3000");
 
   // ======================
   // 🔥 FETCH DATA
